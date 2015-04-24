2d9c61202941ac1a2a84c6d1570a8c7baecf641e
hamidabbaszadeh <abbaszadeh.h@gmail.com>
2015-04-24 20:24:40 +0430

    fix

--- languages/fa/modules.php
+++ languages/fa/modules.php
@@ -3,11 +3,11 @@
 /**
  * Back end modules
  */
-$GLOBALS['TL_LANG']['MOD']['athletes'] = array('اعضاء گروه', 'افزودن و مدیریت اعضاء.');
+$GLOBALS['TL_LANG']['MOD']['athletes'] = array('ورزشکاران', 'افزودن و مدیریت گروه‌ها و ورزشکاران.');
 
 
 /**
  * Front end modules
  */
-$GLOBALS['TL_LANG']['FMD']['athletes_list']   = array('لیست اعضاء', 'افزودن لیست اعضاء به وب‌سایت.');
-$GLOBALS['TL_LANG']['FMD']['athletes_reader'] = array('جزئیات عضو', 'افزودن صفحه جزئیات عضو به وب‌سایت.');
+$GLOBALS['TL_LANG']['FMD']['athletes_list']   = array('لیست ورزشکاران', 'افزودن لیست ورزشکاران به وب‌سایت.');
+$GLOBALS['TL_LANG']['FMD']['athletes_reader'] = array('جزئیات ورزشکار', 'افزودن صفحه جزئیات ورزشکار به وب‌سایت.');
