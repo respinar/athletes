27980292fb8c34b3b59d28c21719a0035fef6864
hamidabbaszadeh <abbaszadeh.h@gmail.com>
2015-07-10 11:54:11 +0430

    Renaming reader and group

--- config/config.php
+++ config/config.php
@@ -1,13 +1,21 @@
 <?php
 
 /**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+/**
  * Back end modules
  */
 array_insert($GLOBALS['BE_MOD']['content'], 1, array
 (
 	'athletes' => array
 	(
-		'tables' => array('tl_athletes_group','tl_athletes', 'tl_athletes_act'),
+		'tables' => array('tl_athletes_category','tl_athletes', 'tl_content'),
 		'icon'   => 'system/modules/athletes/assets/icon.png'
 	)
 ));
@@ -17,4 +25,4 @@ array_insert($GLOBALS['BE_MOD']['content'], 1, array
  * Front end modules
  */
 $GLOBALS['FE_MOD']['athletes']['athletes_list']   = 'ModuleAthletesList';
-$GLOBALS['FE_MOD']['athletes']['athletes_reader'] = 'ModuleAthletesReader';
+$GLOBALS['FE_MOD']['athletes']['athletes_detail'] = 'ModuleAthletesDetail';
