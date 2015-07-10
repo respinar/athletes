6449150d581600b10092b2ffe57f30da3bbab101
hamidabbaszadeh <abbaszadeh.h@gmail.com>
2015-07-10 11:53:05 +0430

    Rename group to category

--- languages/en/tl_athletes_group.php
+++ languages/en/tl_athletes_group.php
@@ -3,24 +3,26 @@
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_athletes_group']['title']       = array('Title', 'Please enter the group title.');
-$GLOBALS['TL_LANG']['tl_athletes_group']['alias']       = array('Alias', 'Please enter the alias.');
-$GLOBALS['TL_LANG']['tl_athletes_group']['description'] = array('Description', 'Here you can enter the group description.');
+$GLOBALS['TL_LANG']['tl_athletes_category']['title']     = array('Title', 'Please enter the group title.');
+$GLOBALS['TL_LANG']['tl_athletes_category']['jumpTo']    = array('Redirect page', 'Please choose the athlete detail page to which visitors will be redirected when clicking a news item.');
+$GLOBALS['TL_LANG']['tl_athletes_category']['protected'] = array('Protect archive', 'Show athlete items to certain member groups only.');
+$GLOBALS['TL_LANG']['tl_athletes_category']['groups']    = array('Allowed member groups','These groups will be able to see the athlete items in this archive.');
 
 
 /**
  * Legends
  */
-$GLOBALS['TL_LANG']['tl_athletes_group']['title_legend'] = 'Title and Alias';
-$GLOBALS['TL_LANG']['tl_athletes_group']['cover_legend'] = 'Description';
+$GLOBALS['TL_LANG']['tl_athletes_category']['title_legend']     = 'Title and Alias';
+$GLOBALS['TL_LANG']['tl_athletes_category']['redirect_legend']  = 'Redirect settings';
+$GLOBALS['TL_LANG']['tl_athletes_category']['protected_legend'] = 'Access protection';
 
 
 /**
  * Buttons
  */
-$GLOBALS['TL_LANG']['tl_athletes_group']['new']        = array('New group', 'Add a new group');
-$GLOBALS['TL_LANG']['tl_athletes_group']['show']       = array('Group details', 'Show the details of group ID %s');
-$GLOBALS['TL_LANG']['tl_athletes_group']['edit']       = array('Edit group', 'Edit group ID %s');
-$GLOBALS['TL_LANG']['tl_athletes_group']['editheader'] = array('Edit group settings', 'Edit group settings ID %s');
-$GLOBALS['TL_LANG']['tl_athletes_group']['copy']       = array('Copy group', 'Copy group ID %s');
-$GLOBALS['TL_LANG']['tl_athletes_group']['delete']     = array('Delete group', 'Delete group ID %s');
+$GLOBALS['TL_LANG']['tl_athletes_category']['new']        = array('New group', 'Add a new group');
+$GLOBALS['TL_LANG']['tl_athletes_category']['show']       = array('Group details', 'Show the details of group ID %s');
+$GLOBALS['TL_LANG']['tl_athletes_category']['edit']       = array('Edit group', 'Edit group ID %s');
+$GLOBALS['TL_LANG']['tl_athletes_category']['editheader'] = array('Edit group settings', 'Edit group settings ID %s');
+$GLOBALS['TL_LANG']['tl_athletes_category']['copy']       = array('Copy group', 'Copy group ID %s');
+$GLOBALS['TL_LANG']['tl_athletes_category']['delete']     = array('Delete group', 'Delete group ID %s');
--- languages/fa/tl_athletes_group.php
+++ languages/fa/tl_athletes_group.php
@@ -3,24 +3,24 @@
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_athletes_group']['title']       = array('عنوان', 'Please enter the group title.');
-$GLOBALS['TL_LANG']['tl_athletes_group']['alias']       = array('مستعار', 'Please enter the alias.');
-$GLOBALS['TL_LANG']['tl_athletes_group']['description'] = array('توضیح', 'Here you can enter the group description.');
+$GLOBALS['TL_LANG']['tl_athletes_category']['title']       = array('عنوان', 'Please enter the group title.');
+$GLOBALS['TL_LANG']['tl_athletes_category']['alias']       = array('مستعار', 'Please enter the alias.');
+$GLOBALS['TL_LANG']['tl_athletes_category']['description'] = array('توضیح', 'Here you can enter the group description.');
 
 
 /**
  * Legends
  */
-$GLOBALS['TL_LANG']['tl_athletes_group']['title_legend'] = 'عنوان و مستعار';
-$GLOBALS['TL_LANG']['tl_athletes_group']['description_legend'] = 'توضیحات';
+$GLOBALS['TL_LANG']['tl_athletes_category']['title_legend'] = 'عنوان و مستعار';
+$GLOBALS['TL_LANG']['tl_athletes_category']['description_legend'] = 'توضیحات';
 
 
 /**
  * Buttons
  */
-$GLOBALS['TL_LANG']['tl_athletes_group']['new']        = array('گروه جدید', 'Add a new group');
-$GLOBALS['TL_LANG']['tl_athletes_group']['show']       = array('جزئیات گروه', 'Show the details of group ID %s');
-$GLOBALS['TL_LANG']['tl_athletes_group']['edit']       = array('ویرایش گروه', 'Edit group ID %s');
-$GLOBALS['TL_LANG']['tl_athletes_group']['editheader'] = array('ویرایش تنظیمات گروه', 'Edit group settings ID %s');
-$GLOBALS['TL_LANG']['tl_athletes_group']['copy']       = array('رونوشت گروه', 'Copy group ID %s');
-$GLOBALS['TL_LANG']['tl_athletes_group']['delete']     = array('حذف گروه', 'Delete group ID %s');
+$GLOBALS['TL_LANG']['tl_athletes_category']['new']        = array('گروه جدید', 'Add a new group');
+$GLOBALS['TL_LANG']['tl_athletes_category']['show']       = array('جزئیات گروه', 'Show the details of group ID %s');
+$GLOBALS['TL_LANG']['tl_athletes_category']['edit']       = array('ویرایش گروه', 'Edit group ID %s');
+$GLOBALS['TL_LANG']['tl_athletes_category']['editheader'] = array('ویرایش تنظیمات گروه', 'Edit group settings ID %s');
+$GLOBALS['TL_LANG']['tl_athletes_category']['copy']       = array('رونوشت گروه', 'Copy group ID %s');
+$GLOBALS['TL_LANG']['tl_athletes_category']['delete']     = array('حذف گروه', 'Delete group ID %s');
